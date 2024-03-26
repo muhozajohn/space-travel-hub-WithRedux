674ea7422ca205e640fc2bea03ae3db81a350f44
Muhoza John <muhozajohn250@gmail.com>
2024-03-26 12:32:59 +0200

    Adding Profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import RocketsIndex from "./components/features/rocket/rocket";
-
+import { Routes, Route } from 'react-router-dom';
+import Navbar from "./components/navbar/Navbar";
+import ProfilePage from "./components/features/profile/profile";
 export default function App() {
   return (
     <div className="flex flex-col gap-5 ">
-      <RocketsIndex />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<RocketsIndex />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 
-const Button = () => {
+const Button = ({ path, click, title, icon, styles }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <Link
+      to={path}
+      onClick={click}
+      className={`${styles} border border-green-700 text-green-700 px-6 py-3 rounded-full flex justify-center items-center gap-2 text-sm hover:bg-green-700 hover:text-white duration-150 hover:scale-100 scale-95`}
+    >
+      {title} {icon}
+    </Link>
+  );
+};
 
-export default Button
+export default Button;
